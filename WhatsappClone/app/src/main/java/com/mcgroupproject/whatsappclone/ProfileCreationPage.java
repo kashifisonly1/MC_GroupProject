112f32a7f2f2b5176edc634dd80d0374030b0f2e
kashif tariq <kashift99@gmail.com>
2021-05-12 17:27:53 +0500

    firestore database added, for users UID and phone numbers saving and searching

--- WhatsappClone/app/src/main/java/com/mcgroupproject/whatsappclone/ProfileCreationPage.java
+++ WhatsappClone/app/src/main/java/com/mcgroupproject/whatsappclone/ProfileCreationPage.java
@@ -11,22 +11,39 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ProfileCreationPage extends AppCompatActivity {
 
     FirebaseAuth mAuth;
     EditText textView;
+    FirebaseFirestore db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_creation_page);
         mAuth = FirebaseAuth.getInstance();
         textView = findViewById(R.id.nameField);
+        db = FirebaseFirestore.getInstance();
         textView.setText(mAuth.getCurrentUser().getDisplayName());
-
     }
 
     public void setName(View view) {
@@ -38,12 +55,53 @@ public class ProfileCreationPage extends AppCompatActivity {
             @Override
             public void onComplete(@NonNull Task<Void> task) {
                 if(task.isSuccessful()){
-                    Intent intent = new Intent(getBaseContext(), MainActivity.class);
-                    startActivity(intent);
+                    Map<String, Object> user = new HashMap<String, Object>();
+                    user.put("UID", mAuth.getUid());
+                    user.put("Name", mAuth.getCurrentUser().getDisplayName());
+                    user.put("Phone", mAuth.getCurrentUser().getPhoneNumber());
+                    updateDatabase(user);
                 }else {
                     Toast.makeText(getApplicationContext(), "Could not change name", Toast.LENGTH_LONG).show();
                 }
             }
         });
+    }
+    private void updateDatabase(Map<String, Object> user)
+    {
+        db.collection("users")
+        .whereEqualTo("UID", user.get("UID"))
+        .get()
+        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    if(task.getResult().size()!=0) {
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+                            document.getReference().update(user);
+                            Intent intent = new Intent(getBaseContext(), MainActivity.class);
+                            startActivity(intent);
+                        }
+                    }
+                    else{
+                        db.collection("users").add(user)
+                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                            @Override
+                            public void onSuccess(DocumentReference documentReference) {
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(getApplicationContext(), "Could not update name", Toast.LENGTH_LONG).show();
+                            }
+                        });
+                    }
+                } else {
+                    Toast.makeText(getApplicationContext(), "Could not update name", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+
     }
 }
