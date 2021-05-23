b7036eca8816bd2d1d470bb6c2140627d467028b
GullSherKhan <79886689+GullSherKhan@users.noreply.github.com>
2021-05-23 22:27:13 +0500

    Chat Screen fixed
    errors fixed
    send button-disappeared-fixed
    java file-disappeared-fixed

---
+++ WhatsappClone/app/src/main/java/com/mcgroupproject/whatsappclone/activity_message.java
@@ -0,0 +1,15 @@
+package com.mcgroupproject.whatsappclone;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class activity_message extends Activity {
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_message);
+
+    }
+}
