7db6302de5b2f8f9bcdb81b08212509ded63882a
GullSherKhan <79886689+GullSherKhan@users.noreply.github.com>
2021-05-24 00:32:27 +0500

    errors fixed+basic functionality added
    functionality added to menu item- new message
    errors fixed--> ids fixed, new ids added

--- WhatsappClone/app/src/main/java/com/mcgroupproject/whatsappclone/activity_main_latest.java
+++ WhatsappClone/app/src/main/java/com/mcgroupproject/whatsappclone/activity_main_latest.java
@@ -1,14 +1,40 @@
 package com.mcgroupproject.whatsappclone;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import org.jetbrains.annotations.NotNull;
 
 public class activity_main_latest extends AppCompatActivity {
-
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_latest);
+        bottomNavigationView = findViewById(R.id.bnv);
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.action_chat:
+
+                            case R.id.action_new_message:
+                                Intent intent = new Intent(getBaseContext(), activity_new_message.class);
+                                startActivity(intent);
+                            case R.id.action_search:
+
+                            case R.id.action_settings:
+
+                        }
+                        return true;
+                    }
+                });
     }
 }
--- WhatsappClone/app/src/main/java/com/mcgroupproject/whatsappclone/activity_new_message.java
+++ WhatsappClone/app/src/main/java/com/mcgroupproject/whatsappclone/activity_new_message.java
@@ -1,14 +1,38 @@
 package com.mcgroupproject.whatsappclone;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class activity_new_message extends AppCompatActivity {
-
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_message);
+        bottomNavigationView = findViewById(R.id.bnv2);
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.action_back:
+                                Intent intent = new Intent(getBaseContext(), activity_main_latest.class);
+                                startActivity(intent);
+                            case R.id.action_new_contact:
+
+                            case R.id.action_new_group:
+
+                            case R.id.action_search:
+
+                        }
+                        return true;
+                    }
+                });
     }
 }
