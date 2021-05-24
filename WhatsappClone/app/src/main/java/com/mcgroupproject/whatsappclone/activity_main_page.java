197db975306472265acdfd1c25390e742cf2587e
Saam J. Z3R0 <bsef18m038@pucit.edu.pk>
2021-05-24 08:53:31 +0500

    Main Screen Register Login Verificaation Screen General

---
+++ WhatsappClone/app/src/main/java/com/mcgroupproject/whatsappclone/activity_main_page.java
@@ -0,0 +1,15 @@
+package com.mcgroupproject.whatsappclone;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class activity_main_page extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_page);
+    }
+}
+
