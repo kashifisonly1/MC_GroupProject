c7b9e27be45d19e404a23b4d06a9b61372cbb54d
GullSherKhan <79886689+GullSherKhan@users.noreply.github.com>
2021-05-24 15:02:55 +0500

    Search screen added + minor fixes
    activity_main_latest, activity_new_message navigation bar items intents removed due to error
    icons,-search_white added
    activity_Search and its java file added

--- WhatsappClone/app/src/main/java/com/mcgroupproject/whatsappclone/activity_new_message.java
+++ WhatsappClone/app/src/main/java/com/mcgroupproject/whatsappclone/activity_new_message.java
@@ -10,29 +10,11 @@ import android.view.MenuItem;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class activity_new_message extends AppCompatActivity {
-    BottomNavigationView bottomNavigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_message);
-        bottomNavigationView = findViewById(R.id.bnv2);
-        bottomNavigationView.setOnNavigationItemSelectedListener(
-                new BottomNavigationView.OnNavigationItemSelectedListener() {
-                    @Override
-                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                        switch (item.getItemId()) {
-                            case R.id.action_back:
-                                Intent intent = new Intent(getBaseContext(), activity_main_latest.class);
-                                startActivity(intent);
-                            case R.id.action_new_contact:
 
-                            case R.id.action_new_group:
-
-                            case R.id.action_search:
-
-                        }
-                        return true;
-                    }
-                });
     }
 }
--- WhatsappClone/app/src/main/java/com/mcgroupproject/whatsappclone/search.java
+++ WhatsappClone/app/src/main/java/com/mcgroupproject/whatsappclone/search.java
@@ -9,13 +9,12 @@ import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import org.jetbrains.annotations.NotNull;
-
-public class activity_main_latest extends AppCompatActivity {
+public class search extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_latest);
+        setContentView(R.layout.activity_search);
+        getSupportActionBar().hide();
 
     }
 }
