4254c6d22d1c0e89ad2ce943601c7396432bd462
saimbintalib <49138179+saimbintalib@users.noreply.github.com>
2021-05-25 14:15:25 +0500

    Sign Up Page designed

--- WhatsappClone/app/src/main/java/com/mcgroupproject/whatsappclone/activity_main_page.java
+++ WhatsappClone/app/src/main/java/com/mcgroupproject/whatsappclone/activity_main_page.java
@@ -9,7 +9,7 @@ public class activity_main_page extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_page);
+        setContentView(R.layout.activity_main);
     }
 }
 
