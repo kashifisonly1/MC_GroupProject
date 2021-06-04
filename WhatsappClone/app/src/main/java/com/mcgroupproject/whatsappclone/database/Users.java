377f734bd6600769788191c0c33c75bf8d089178
Saam J. Z3R0 <bsef18m038@pucit.edu.pk>
2021-06-04 23:48:09 +0500

    Db ...

--- WhatsappClone/app/src/main/java/com/mcgroupproject/whatsappclone/database/Users.java
+++ WhatsappClone/app/src/main/java/com/mcgroupproject/whatsappclone/database/Users.java
@@ -1,44 +1,45 @@
+
 package com.mcgroupproject.whatsappclone.database;
 
-import com.mcgroupproject.whatsappclone.model.UserModel;
+import com.mcgroupproject.whatsappclone.model.User;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class Users {
-    public static final List<UserModel> list = new ArrayList<>();
-    public static boolean Add(UserModel i)
-    {
-        for(int a = 0; a<list.size();a++)
-        {
-            if(list.get(a).getUserID().equals(i.getUserID()))
-                return false;
+public class UserDB {
+    public static final List<User> list = new ArrayList<>();
+
+    public static boolean Add(User i) {
+        for (int a = 0; a < list.size(); a++) {
+            list.add(i);
+            return true;
         }
-        list.add(i);
-        return true;
-    }
-    public static void Remove(UserModel i)
-    {
-        //TODO: remove user
-        list.remove(i);
-    }
-    public static void Update(UserModel i)
-    {
-        //TODO: update user
-        list.set(list.indexOf(i), i);
-    }
-    public static List<UserModel> Get()
-    {
-        List<UserModel> l  = new ArrayList<>();
-        for(int i = 0; i<list.size(); i++)
-            l.add(list.get(i));
-        return l;
-    }
-    public static int isUserExist(String id)
-    {
-        for(int i = 0; i<list.size(); i++)
-            if(list.get(i).getUserID().equals(id))
-                return i;
-        return -1;
+        public static void Remove (User i)
+        {
+            //TODO: remove user
+            list.remove(i);
+        }
+        public static void Update (User i)
+        {
+            //TODO: update user
+            list.set(list.indexOf(i), i);
+        }
+
+        public static List<User> Get ()
+        {
+            List<User> l = new ArrayList<>();
+            for (int i = 0; i < list.size(); i++)
+                l.add(list.get(i));
+            return l;
+        }
+
+        public static int isUserExist (String id)
+        {
+            for (int i = 0; i < list.size(); i++)
+                if (list.get(i).getUserID().equals(id))
+                    return i;
+            return -1;
+        }
+
     }
 }
