a490aeff520bb3782e46c5f631cccf106be8bace
Saam J. Z3R0 <bsef18m038@pucit.edu.pk>
2021-06-26 14:28:50 +0500

    user comparision (utilities )

--- WhatsappClone/app/src/main/java/com/mcgroupproject/whatsappclone/Utilities/UserComparison.java
+++ WhatsappClone/app/src/main/java/com/mcgroupproject/whatsappclone/Utilities/UserComparison.java
@@ -4,7 +4,10 @@ import com.mcgroupproject.whatsappclone.Model.User;
 
 import java.util.Comparator;
 
-
+/*
+ * Purpose of this function is to keep user updated.. so that user who send last message alwasy stays on the top,,
+ * for this we are simply comparing the dates of both user's last message
+ * * */
 
 public class UserComparison implements Comparator<User> {
         public int compare(User a, User b)
